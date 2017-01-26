4c6124f6240bbafca105fedf19cb048dc2873b15
Gálffy Tamás <ezittgtx@gmail.com>
2017-01-26 14:20:10 +0200

    Added 2-5 The fun-ish way?

---
+++ excercise 2/multiplication-table.py
@@ -0,0 +1,25 @@
+def int_input(text='', error_text='Not a number!', range=None, range_error_text='Value out of range!'):
+    while True:
+        try:
+            val = input(text)
+            val = int(val)
+
+            if range is not None:
+                if val not in range:
+                    print('{0} ({1}-{2})'.format(range_error_text, range[0], range[-1]))
+                    continue
+
+            return val
+        except ValueError:
+            print(error_text)
+
+def main():
+    limit = int_input('Upper limit> ')
+
+    for i in range(1, limit+1):
+        for j in range(1, limit+1):
+            print('{0} * {1} = {2}'.format(i,j,i*j))
+        print('')
+
+if __name__ == "__main__":
+    main()
